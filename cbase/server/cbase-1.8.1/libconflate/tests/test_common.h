4da023e8750c548815cc2563297d0f54c6c0ba2a
Albert Zhang <zhgwenming@gmail.com>
2013-07-12 16:17:11 +0800

    cbase: remove the actual files

--- cbase/server/cbase-1.8.1/libconflate/tests/test_common.h
+++
@@ -1,8 +0,0 @@
-#ifndef TEST_COMMON_H
-#define TEST_COMMON_H 1
-
-#include <conflate.h>
-
-void check_pair_equality(kvpair_t *one, kvpair_t *two);
-
-#endif /* TEST_COMMMON_H */
--- cbase/server/cbase-1.8.1/libvbucket/win32/inttypes.h
+++
@@ -1,9 +0,0 @@
-/*
- * Just include stdint instead.. it's good enough for us.
- */
-#ifndef INTTYPES_H
-#define INTTYPES_H
-
-#include <stdint.h>
-
-#endif
