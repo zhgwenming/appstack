cf4d1da585cb45f833d4ad145260b1a05169347d
Albert Zhang <zhgwenming@gmail.com>
2013-07-12 21:09:07 +0800

    cbase: memcached as a seperate pkg

---
+++ cbase/memcached/memcached-1.4.4/extensions/daemon/stdin_check.c
@@ -0,0 +1,95 @@
+/* -*- Mode: C; tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil -*- */
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <string.h>
+
+#include "protocol_extension.h"
+
+union c99hack {
+    void *pointer;
+    void (*exit_function)(void);
+};
+
+/*
+ * The stdin_term_handler allows you to shut down memcached from
+ * another process by the use of a pipe. It operates in a line mode
+ * with the following syntax: "command\n"
+ *
+ * The following commands exists:
+ *   shutdown - Request memcached to initiate a clean shutdown
+ *   die!     - Request memcached to die as fast as possible! like
+ *              the unix "kill -9"
+ *
+ * Please note that you may try to shut down cleanly and give
+ * memcached a grace period to complete, and if you don't want to wait
+ * any longer you may send "die!" and have it die immediately. All
+ * unknown commands will be ignored.
+ *
+ * If the input stream is closed a clean shutdown is initiated
+ */
+static void* check_stdin_thread(void* arg)
+{
+    union c99hack chack = { .pointer = arg };
+    pthread_detach(pthread_self());
+
+    char command[80];
+    while (fgets(command, sizeof(command), stdin) != NULL) {
+        /* Handle the command */
+        if (strcmp(command, "die!\n") == 0) {
+            fprintf(stderr, "'die!' on stdin.  Exiting super-quickly\n");
+            fflush(stderr);
+            _Exit(0);
+        } else if (strcmp(command, "shutdown\n") == 0) {
+            if (chack.pointer != NULL) {
+                fprintf(stderr, "EOL on stdin.  Initiating shutdown\n");
+                chack.exit_function();
+                chack.pointer = NULL;
+            }
+        } else {
+            fprintf(stderr, "Unknown command received on stdin.  Ignored\n");
+        }
+    }
+
+    /* The stream is closed.. do a nice shutdown */
+    if (chack.pointer != NULL) {
+        fprintf(stderr, "EOF on stdin.  Initiating shutdown \n");
+        chack.exit_function();
+    }
+
+    /* NOTREACHED */
+    return NULL;
+}
+
+static const char *get_name(void) {
+    return "stdin_check";
+}
+
+static EXTENSION_DAEMON_DESCRIPTOR descriptor = {
+    .get_name = get_name
+};
+
+MEMCACHED_PUBLIC_API
+EXTENSION_ERROR_CODE memcached_extensions_initialize(const char *config,
+                                                     GET_SERVER_API get_server_api) {
+
+    SERVER_HANDLE_V1 *server = get_server_api();
+    if (server == NULL) {
+        return EXTENSION_FATAL;
+    }
+
+    if (!server->extension->register_extension(EXTENSION_DAEMON, &descriptor)) {
+        return EXTENSION_FATAL;
+    }
+
+    union c99hack ch = { .exit_function = server->core->shutdown };
+
+    pthread_t t;
+    if (pthread_create(&t, NULL, check_stdin_thread, ch.pointer) != 0) {
+        perror("couldn't create stdin checking thread.");
+        server->extension->unregister_extension(EXTENSION_DAEMON, &descriptor);
+        return EXTENSION_FATAL;
+    }
+
+    return EXTENSION_SUCCESS;
+}
---
+++ cbase/memcached/memcached-1.4.4/include/memcached/allocator_hooks.h
@@ -0,0 +1,102 @@
+
+/**
+ * Use this file as an abstraction to the underlying hooks api
+ */
+
+#ifndef ALLOCATOR_HOOKS_H
+#define ALLOCATOR_HOOKS_H
+
+#include <stdint.h>
+
+#ifndef __cplusplus
+#include <stdbool.h>
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct allocator_ext_stat {
+    char key[48];
+    size_t value;
+} allocator_ext_stat;
+
+typedef struct allocator_stats {
+    size_t allocated_size;
+    size_t heap_size;
+    size_t free_size;
+    size_t fragmentation_size;
+    allocator_ext_stat *ext_stats;
+    size_t ext_stats_size;
+} allocator_stats;
+
+/**
+ * Engine allocator hooks for memory tracking.
+ */
+typedef struct engine_allocator_hooks_v1 {
+
+    /**
+     * Add a hook into the memory allocator that will be called each
+     * time memory is allocated from the heap. Returns true if the
+     * hook was successfully registered with the allocator. Returns
+     * false if the hook was not registered properly or if a hooks
+     * API doesn't exist for the allocator in use.
+     */
+    bool (*add_new_hook)(void (*)(const void* ptr, size_t size));
+
+    /**
+     * Remove a hook from the memory allocator that will be called each
+     * time memory is allocated from the heap. Returns true if the hook
+     * was registered and removed and false if the specified hook is not
+     * registered or if a hooks API doesn't exist for the allocator.
+     */
+    bool (*remove_new_hook)(void (*)(const void* ptr, size_t size));
+
+    /**
+     * Add a hook into the memory allocator that will be called each
+     * time memory is freed from the heap. Returns true if the hook
+     * was successfully registered with the allocator. Returns false
+     * if the hook was not registered properly or if a hooks API
+     * doesn't exist for the allocator in use.
+     */
+    bool (*add_delete_hook)(void (*)(const void* ptr));
+
+    /**
+     * Remove a hook from the memory allocator that will be called each
+     * time memory is freed from the heap. Returns true if the hook was
+     * registered and removed and false if the specified hook is not
+     * registered or if a hooks API doesn't exist for the allocator.
+     */
+    bool (*remove_delete_hook)(void (*)(const void* ptr));
+
+    /**
+     * Returns the number of extra stats for the current allocator.
+     */
+    int (*get_extra_stats_size)(void);
+
+    /**
+     * Obtains relevant statistics from the allocator. Every allocator
+     * is required to return total allocated bytes, total heap bytes,
+     * total free bytes, and toal fragmented bytes. An allocator will
+     * also provide a varying number of allocator specific stats
+     */
+    void (*get_allocator_stats)(allocator_stats*);
+
+    /**
+     * Returns the total bytes allocated by the allocator. This value
+     * may be computed differently based on the allocator in use.
+     */
+    size_t (*get_allocation_size)(void*);
+
+    /**
+     * Fills a buffer with special detailed allocator stats.
+     */
+    void (*get_detailed_stats)(char*, int);
+
+} ALLOCATOR_HOOKS_API;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* ALLOCATOR_HOOKS_H */
